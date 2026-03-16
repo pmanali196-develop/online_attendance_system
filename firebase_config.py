f8b36176e2b1eb0bc24e7d787879d21d88fe9855
Manali Patel <pmanali196@gmail.com>
2026-03-16 23:12:42 +0530

    Added updated firebase_config file

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate("firebase_key.json")
+# cred = credentials.Certificate("firebase_key.json")
+# firebase_admin.initialize_app(cred)
+
+# db = firestore.client()
+
+firebase_key = json.loads(os.environ["FIREBASE_KEY"])
+
+cred = credentials.Certificate(firebase_key)
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
