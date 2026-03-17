888de68d5fbc8a0861071cd3da66f005042a3c61
Manali Patel <pmanali196@gmail.com>
2026-03-17 21:39:03 +0530

    Changed firebase_key path in firebase_config file

--- firebase_config.py
+++ firebase_config.py
@@ -1,18 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
-# import os
-# import json
+import os
+import json
 
 # cred = credentials.Certificate("firebase_key.json")
 # firebase_admin.initialize_app(cred)
 
 # db = firestore.client()
 
-# firebase_key = json.loads(os.environ["FIREBASE_KEY"])
+firebase_key = json.loads(os.environ["FIREBASE_KEY"])
 
-# cred = credentials.Certificate(firebase_key)
+cred = credentials.Certificate(firebase_key)
 
-cred = credentials.Certificate("firebase_key.json")
+# cred = credentials.Certificate("firebase_key.json")
 
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'face-attendance-system-7fccd.appspot.com'
